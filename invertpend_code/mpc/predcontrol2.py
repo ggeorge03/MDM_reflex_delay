eb3b60fece2ca32da9ba8a7bfe23cf50d97b24c2
Will B <159014836+Will-Buesnel@users.noreply.github.com>
2025-02-28 11:07:26 +0000

    Update predcontrol2.py

--- invertpend_code/mpc/predcontrol2.py
+++ invertpend_code/mpc/predcontrol2.py
@@ -1,8 +1,10 @@
-
 import numpy as np
 from scipy.optimize import minimize
 class MPC:
+    # this is pretty much only useable for an inverted pendulum model atm, but can be adapted for other models.
 
+    # The higher the prediction horizon is, the more optimal solution that will be produced but it will take exponentially longer
+    # also its diminishing returns as you increase it.
 
     def __init__(self,A,B,C,Q,R,pred_hor, cont_hor, desired_state):
         self.A = A # state transition matric
@@ -11,7 +13,7 @@ class MPC:
         self.Q = Q # cost function weight on accuracy
         self.R = R # cost function weight on effort
         self.f = pred_hor # prediction horizon ( no. of steps ahead to go and predict )
-        self.v = cont_hor # control horizon - shouldn't this be for the whole estimated itm
+        self.v = cont_hor # control horizon 
         self.z = np.array(desired_state) # reference trajectory - for this we can treat it as constant but could be adjusted to vary with time.
 
     def set_bounds(self, min, max):
@@ -23,40 +25,42 @@ class MPC:
         self.states_ub  = max
 
     def mpc_cost_function(self,u_sequence):
-        """
+        '''
         Cost function for MPC that heavily penalizes theta deviations.
+        If it didn't penalise theta as much it thinks the most efficient way is to repeatedly swing it up and down.
         
-        Returns:
-            J: Scalar cost value
-        """
+        J: Scalar cost value
+        '''
         # Reshape control sequence
         u = u_sequence.reshape(self.f, -1)
         
-        # Initialize total cost and current state
-        J = 0
-        x = self.x0.copy()
+        J = 0 # total cost
+        x = self.x0.copy() # current state
         theta_max = self.states_ub[2]
+        x_max = self.states_ub[0]
         # Simulate system over prediction horizon
         for k in range(self.f):
-            # State error cost (quadratic)
+            # State error cost 
             state_error = x - self.z
             state_cost = state_error.T @ self.Q @ state_error
             
-            # Control cost (quadratic)
-            control_cost = u[k].T * self.R * u[k]
+            # Control cost 
+            control_cost = u[k].T * self.R * u[k] # scalar multiplication becuase control input is scalar.
             
-            # Special theta penalty (exponential barrier function)
+            # Special penalty for going out of bounds.
             theta = x[2]  # Assuming theta is the 3rd state
+            xpos = x[0]
+            x_ratio = abs(xpos) / x_max
             theta_ratio = abs(theta) / theta_max
-            # Exponential barrier that grows extremely fast as theta approaches limits
-            if theta_ratio > 0.7:
-                barrier_strength = 2000.0  # Higher means stronger constraint enforcement
-                theta_penalty = 10000*theta_ratio
+
+            if (theta_ratio > 0.7 )|(x_ratio > 0.7):
+                
+                penalty = 100*(theta_ratio+x_ratio)**2
             else:
-                theta_penalty = 0
+                penalty = 0
             
             # Sum costs for this time step
-            J += state_cost + control_cost + theta_penalty
+            J += state_cost + control_cost + penalty
             
             # Propagate dynamics to next state
             x = self.dynamics_model(x, u[k])
@@ -65,7 +69,7 @@ class MPC:
     
     def get_opt_control(self, x0):
         self.x0 = np.array(x0)
-        initial_guess = np.ones(self.f)
+        initial_guess = np.ones(self.f)*0.5
         u = minimize(self.mpc_cost_function, initial_guess, method="SLSQP")
         if u.success:
             return u.x
@@ -73,27 +77,33 @@ class MPC:
             raise ValueError("Optimization failed: " + u.message)
 
     def dynamics_model(self, x, u):
-        #print(f"{x=}, {u=}")
+        
         state_change = self.A @ x
         control_change = self.B * u
-        #print(f"{state_change.shape=}, {control_change.flatten().shape=}")
+        
         return state_change + control_change.flatten()
     
         # Runge-Kutta 4th order integration to propagate to next state.
     def runge_kutta(self, x, u, dt):
+        '''
+        Apparantly Runge-kutta integration is more accurate than Euler
+        Euler integration being x_n+1 = x_n + x_dot_n * dt
+        '''
         k1 = self.dynamics_model(x, u)
         k2 = self.dynamics_model(x + 0.5 * k1 * dt, u)
         k3 = self.dynamics_model(x + 0.5 * k2 * dt, u)
         k4 = self.dynamics_model(x + k3 * dt, u)
         return x + (k1 + 2*k2 + 2*k3 + k4) * dt / 6
     
-    def sim_model(self,x0,n_iter=100, dt=0.1):
+    def sim_model(self,x0,n_iter=100, dt=0.1,delay=1):
         states = [x0]
         control_inputs = []
         x = x0
-        for _ in range(n_iter):
-            u_opt = self.get_opt_control(x)
-            u_opt = [0] # currently doing no control just to try and see where the error is..
+        for k in range(n_iter):
+            if k > delay:
+                u_opt = self.get_opt_control(states[k-delay])
+            else: u_opt = [0]
+            #u_opt = [0]
             u_taken = np.array([u_opt[0]])
             x = self.runge_kutta(x, u_taken,dt)
             states.append(x)
@@ -132,7 +142,7 @@ def initialise_variables():
 
 
     
-if __name__ == "__main__":
+if __name__ == "__main__": # for testing..
 
     # linearise system around equilibrium point theta = 0.
     matricies = initialise_variables()
