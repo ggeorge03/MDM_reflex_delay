7068cced67984a3497bd8108b53b49b078bd5568
ggeorge03 <georgeguillain@gmail.com>
2025-03-11 20:05:55 +0000

    added pvalue

--- Experiment analysis/experiment.py
+++ Experiment analysis/experiment.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import pearsonr
 
 reactions = np.array([188, 267, 217, 297, 267, 263, 267,
                      316, 253, 311, 250, 328, 193, 273, 232])
@@ -11,9 +12,10 @@ p = np.polyfit(reactions, lengths, 1)  # fit degree 1 polynomial to data
 r = np.linspace(np.min(reactions), np.max(reactions), 100)
 fits = p[0] * r + p[1]  # generate points from the fit to plot
 
-# Calculate correlation coefficient
-correlation_coefficient = np.corrcoef(reactions, lengths)[0, 1]
+# Calculate correlation coefficient and p-value
+correlation_coefficient, p_value = pearsonr(reactions, lengths)
 print(f"Correlation Coefficient: {correlation_coefficient:.4f}")
+print(f"P-value: {p_value:.4f}")
 
 # Plot data and fit line
 plt.scatter(reactions, lengths, label='Datapoints')
